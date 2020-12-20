a645ce1fab733bb98a5a10b45a2182a7728ba613
Akila Mendis <akila@oneit.com.au>
2020-12-20 09:22:38 +0530

    first commit

---
+++ src/NewNoteInput.tsx
@@ -0,0 +1,29 @@
+import React, { ChangeEvent } from 'react';
+
+interface NewNoteInputProps {
+    addNote(note:string): void;
+}
+
+
+const NewNoteInput:React.FC<NewNoteInputProps> = ({addNote}) => {
+    const [note, setNote] = React.useState("");
+
+    const updateNote = (event:ChangeEvent<HTMLInputElement>) => {
+        setNote(event.target.value);
+    }
+
+    const onAddNoteClick = () => {
+        addNote(note);
+        setNote("");
+    }
+
+    return(
+        <div>
+            <input onChange={updateNote} value={note} type="text" name="note" placeholder="note"></input>
+            <button onClick={onAddNoteClick}>Add Note</button>
+        </div>
+    );
+
+}
+
+export default NewNoteInput;
